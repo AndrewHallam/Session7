00c11006e5390e893094e87bcb997000a1a6fdf9
Andrew Hallam <andrew.hallam.10@ucl.ac.uk>
2015-02-28 22:01:03 +0000

    Created add method

--- Units/Units.py
+++ Units/Units.py
@@ -10,10 +10,17 @@ class Quantity(object):
          else: 
              raise TypeError("You need to use a number!")
              
+     def add(self, other):
+         if self.unit==other.unit:
+             return Quantity(self.number+other.number, self.unit)
+         else:
+             return TypeError("You can't add things with different units!")  
+             
 class Expression(object):
      def __init__(self, terms):
          self.terms=terms
                  
-first=Quantity('a','meter')
-print type(first.number)
-print first.unit
+first=Quantity(1,'gram')
+second=Quantity(2,'gram')
+third=first.add(second)
+print third.number
