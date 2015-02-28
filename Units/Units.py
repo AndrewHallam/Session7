4b9d1be17eea473d1e54397bbdb57402f0e5f833
Andrew Hallam <andrew.hallam.10@ucl.ac.uk>
2015-02-28 17:52:47 +0000

    Created a quantity class. Will raise an error if a number isn't put in to the unit.

--- Units/Units.py
+++ Units/Units.py
@@ -1,1 +1,19 @@
-# Will create a unit class here
+class Quantity(object):
+     def __init__(self, number, unit):
+         self.number=number
+         self.unit=unit
+         
+         if type(number)==int:
+             pass
+         elif type(number)==float:
+             pass
+         else: 
+             raise TypeError("You need to use a number!")
+             
+class Expression(object):
+     def __init__(self, terms):
+         self.terms=terms
+                 
+first=Quantity('a','meter')
+print type(first.number)
+print first.unit
